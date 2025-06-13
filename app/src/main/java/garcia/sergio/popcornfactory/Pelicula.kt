f2c26340325f6cfd177c4b375d4e43480f78eb59
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2025-06-13 15:37:18 -0700

    Commit inicial

---
+++ app/src/main/java/garcia/sergio/popcornfactory/Pelicula.kt
@@ -0,0 +1,7 @@
+package garcia.sergio.popcornfactory
+
+data class Pelicula (var titulo: String,
+                     var image: Int,
+                     var header: Int,
+                     var sinopsis: String
+)
